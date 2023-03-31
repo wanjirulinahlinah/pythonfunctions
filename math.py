f14b6e7ddf657010b0adcd202d70acdfebd95b3b
wainainalinahlinah <wainainalinahw@gmail.com>
2023-03-31 21:40:43 +0300

    first commit

---
+++ math.py
@@ -0,0 +1,21 @@
+def add(a,b):
+    answer = a + b
+    return answer
+
+def subtract(d,e):
+    answer = d - e
+    return answer
+
+def divide(m,k):
+    answer = m / k
+    return answer
+
+def mutiply(n,l):
+    answer = n*l
+    return answer 
+
+def reminder(p,l):
+    answer = p % l
+    return answer      
+     
+     
